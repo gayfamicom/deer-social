5ee1a4159610b75bf638a088df1af884588d0045
Niko <gayfamicom@protonmail.com>
2025-09-07 21:27:50 -0700

    changing the deer icons to purple

--- src/view/com/auth/SplashScreen.web.tsx
+++ src/view/com/auth/SplashScreen.web.tsx
@@ -166,7 +166,7 @@ function Footer() {
       ]}>
       <InlineLinkText
         label={_(msg`Read the patches and contribute`)}
-        to="https://github.com/danielah05/deer-social">
+        to="https://github.com/gayfamicom/deer-social">
         <Trans>Github</Trans>
       </InlineLinkText>
 
